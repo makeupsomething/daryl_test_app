033b6bf03e8bc6d53f4d2e38861498ed3e072d1c
daryl <darylcoleoffice@gmail.com>
2016-12-20 15:33:12 +0900

    add weather app

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import time
+import sys
+#sys.path.append("/usr/local/lib/python2.7/site-packages/")
+import requests
+from pprint import pprint
 
 while True:
-	print("Hello IoT from daryl")
-	time.sleep(5)
-
+	r = requests.get('http://api.openweathermap.org/data/2.5/weather?id=2965535&APPID=fff6ce2f865b03b789b7c1f537b6518b')
+	pprint(r.json())
+	time.sleep(5000)
