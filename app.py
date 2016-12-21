0030f7be33756b4a9fc48a5abdb4b0d9e36571d8
daryl <darylcoleoffice@gmail.com>
2016-12-21 18:11:36 +0900

    app update

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ while True:
 	r = requests.get('http://api.openweathermap.org/data/2.5/weather?id=2965535&APPID=fff6ce2f865b03b789b7c1f537b6518b')
 	data = r.json()
 	place = data["name"]
+	country = data['sys']['country']
 	curWeather = data["weather"][0]["main"]
 	curTemp = int(data["main"]["temp"]) - 273.15
-	display = 'The current weather in {0} is {1} and the tempature is {2} celsius'.format(place, curWeather, curTemp)
+	display = 'The current weather in {0},{3} is {1} and the tempature is {2} celsius'.format(place, curWeather, curTemp, country)
 	pprint(display)
 	time.sleep(30)
