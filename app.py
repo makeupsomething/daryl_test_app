0c8fb825a6b3de27be126f7c5820eff8ee9dd1db
makeupsomething <darylcoleoffice@gmail.com>
2017-03-22 19:34:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ from pprint import pprint
 
 while True:
 	try:
-		#r = requests.get('http://api.openweathermap.org/data/2.5/weather?id=2965535&APPID=fff6ce2f865b03b789b7c1f537b6518b')
-		#data = r.json()
-		#place = data["name"]
-		#country = data['sys']['country']
-		#curWeather = data["weather"][0]["main"]
-		#curTemp = int(data["main"]["temp"]) - 273.15
-		#display = 'The current weather in {0},{3} is {1} and the tempature is {2} celsius. Have a nice day.'.format(place, curWeather, curTemp, country)
-		#pprint(display)
-		pprint("daryl test)
+		r = requests.get('http://api.openweathermap.org/data/2.5/weather?id=2965535&APPID=fff6ce2f865b03b789b7c1f537b6518b')
+		data = r.json()
+		place = data["name"]
+		country = data['sys']['country']
+		curWeather = data["weather"][0]["main"]
+		curTemp = int(data["main"]["temp"]) - 273.15
+		display = 'The current weather in {0},{3} is {1} and the tempature is {2} celsius. Have a nice day.'.format(place, curWeather, curTemp, country)
+		pprint(display)
+		#pprint("daryl test)
 	except Exception as ex1:
 		print 'WEATHER DATA ERORR! ' + str(ex1)
-	time.sleep(1)
+	time.sleep(300)
