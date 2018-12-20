06e99defbd36c401b68df56999bb932e7c745f67
makeupsomething <darylcoleoffice@gmail.com>
2018-12-20 11:50:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import requests
 import datetime
 from pprint import pprint
 
+pprint("Starting App")
+
 while True:
 	try:
 		r = requests.get('http://api.openweathermap.org/data/2.5/weather?id=2965535&APPID=fff6ce2f865b03b789b7c1f537b6518b')
@@ -15,7 +17,8 @@ while True:
 		nowString = now.strftime("%I:%M%p on %B %d, %Y %s")
 		display = 'TEST_4 The current weather at {4} in {0},{3} is {1} and the tempature is {2} celsius'.format(place, curWeather, curTemp, country, nowString)
 		pprint(display)
-		#pprint("daryl test)
 	except Exception as ex1:
 		print 'WEATHER DATA ERORR! ' + str(ex1)
 	time.sleep(500)
+
+pprint("Stopping App")
