46fedec12e7932116484a643361e07bc2ce186d5
makeupsomething <darylcoleoffice@gmail.com>
2019-01-10 09:39:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,20 +2,31 @@ import time
 import requests
 import datetime
 from pprint import pprint
+import atexit
+
+pprint("Starting App")
+
+def goodbye():
+	now = datetime.datetime.now()
+	nowString = now.strftime("%I:%M%p on %B %d, %Y %s")
+	pprint('Stopping at ' + nowString)
+
+atexit.register(goodbye)
 
 while True:
 	try:
-		r = requests.get('http://api.openweathermap.org/data/2.5/weather?id=2965535&APPID=fff6ce2f865b03b789b7c1f537b6518b')
+		r = requests.get('http://api.openweathermap.org/data/2.5/weather?id=1850147&APPID=fff6ce2f865b03b789b7c1f537b6518b')
 		data = r.json()
 		place = data["name"]
 		country = data['sys']['country']
 		curWeather = data["weather"][0]["main"]
 		curTemp = int(data["main"]["temp"]) - 273.15
 		now = datetime.datetime.now()
-		nowString = now.strftime("%I:%M%p on %B %d, %Y")
-		display = '[DEV]The current weather at {4} in {0},{3} is {1} and the tempature is {2} celsius'.format(place, curWeather, curTemp, country, nowString)
+		nowString = now.strftime("%I:%M%p on %B %d, %Y %s")
+		display = 'TEST_4 The current weather at {4} in {0},{3} is {1} and the tempature is {2} celsius'.format(place, curWeather, curTemp, country, nowString)
 		pprint(display)
-		#pprint("daryl test)
 	except Exception as ex1:
 		print 'WEATHER DATA ERORR! ' + str(ex1)
-	time.sleep(100030)
+	time.sleep(500)
+
+pprint("Stopping App")
